178082be0c0d4d79b10584f7f9b1ffa0f4a56ad1
pejasco <pejasco@gmail.com>
2024-07-09 19:53:04 +0100

    start

---
+++ ex00/Animal.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chuleung <chuleung@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/09 17:24:10 by chuleung          #+#    #+#             */
+/*   Updated: 2024/07/09 19:52:46 by chuleung         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+class Animal
+{
+    Animal();
+    ~Animal();
+    Animal(const Animal&other);
+    Animal& operator-(const Amina)
+
+
+
+
+};
