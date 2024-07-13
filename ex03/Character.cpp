7b791826f7f092139721e321fdef17342b1c3ff5
pejasco <pejasco@gmail.com>
2024-07-13 23:38:11 +0100

    yes

--- ex03/Character.cpp
+++ ex03/Character.cpp
@@ -6,7 +6,7 @@
 /*   By: chuleung <chuleung@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/13 17:18:48 by chuleung          #+#    #+#             */
-/*   Updated: 2024/07/13 23:07:42 by chuleung         ###   ########.fr       */
+/*   Updated: 2024/07/13 23:37:59 by chuleung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 #include <iostream>
 #include "Character.hpp"
 
-Character::Character(const std::string& name) : name_(name), n
+Character::Character(const std::string& name) : name_(name), nbr_of_unequip_(0)
 {
     for (int i = 0; i < max_slots; ++i)
         inventory_slots_[i] = NULL;
@@ -34,6 +34,7 @@ Character::~Character()
 
 Character::Character(const Character& other) : Character(other.name_)
 {
+    nbr_of_unequip_ = other.
     for (int i = 0; i < max_slots; ++i)
     {
          if (other.inventory_slots_[i] == NULL)
