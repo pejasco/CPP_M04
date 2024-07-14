6c86bbaecaea2ad341044b2111a2d27363630f9a
pejasco <pejasco@gmail.com>
2024-07-14 23:59:56 +0100

    hey

--- ex03/MateriaSource.cpp
+++ ex03/MateriaSource.cpp
@@ -6,7 +6,7 @@
 /*   By: chuleung <chuleung@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/13 17:19:59 by chuleung          #+#    #+#             */
-/*   Updated: 2024/07/14 23:22:23 by chuleung         ###   ########.fr       */
+/*   Updated: 2024/07/14 23:59:34 by chuleung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,3 +60,50 @@ MateriaSource& MateriaSource::operator=(const MateriaSource& other)
     return *this;
 }
 
+void MateriaSource::learnMateria(AMateria* materia)
+{
+    AMateria* clone = 
+
+
+}
+
+AMateria* MateriaSource::createMateria(std::string const& type)
+{
+
+
+
+
+}
+
+
+void MateriaSource::learnMateria(AMateria* materia)
+{
+    std::cout << "MateriaSource: learning materia: " << materia->getType()
+              <<".\n" ;
+    if (!materia)
+        return;
+    const std::string materia_type = materia->getType();
+    for (int i = 0; i < IMateriaSource::materia_max_count_; ++i)
+    {
+        if (materia_slots_[i])
+            continue;
+        else
+        {
+            materia_slots_[i] = materia;
+            break;
+        }
+    }
+    std::cout << "MateriaSource: learning materia complete.\n" ;
+}
+
+AMateria* MateriaSource::createMateria(std::string const& type)
+{
+    std::cout << "MateriaSource: creating materia.\n" ;
+    for (int i = 0; i < IMateriaSource::materia_max_count_; ++i)
+    {
+        if (materia_slots_[i]->getType() == type)
+            return materia_slots_[i]->clone();
+    }
+    std::cout << "MateriaSource: creating materia complete.\n" ;
+    return NULL;
+}
